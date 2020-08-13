73f6ba0b2fb6c76d945438c6495764cf19190103
Nolan Terpstra <nbakert@gmail.com>
2020-08-12 21:28:17 -0500

    Code functional, and sends daily tasks to local storage but have not set up the retrieval. Safe revert point.

---
+++ script.js
@@ -0,0 +1,71 @@
+// Variables
+const date = $("#currentDay")
+let textArea = document.querySelectorAll("textarea");
+const now = moment().format("MMM Do YYYY");
+const clock = parseInt(moment().format('H'));
+
+//Today's date shown on top of screen
+date.text(now);
+// Checks time and colorizes the time boxes
+for(let i=0; i < textArea.length; i++){
+    let textArea = document.querySelectorAll("textarea");
+    let timeColor = parseInt(textArea[i].id)
+    if (timeColor < clock) {
+        textArea[i].classList.add("past");
+    }
+    else if (timeColor > clock) {
+        textArea[i].classList.add("future");
+    }
+    else {
+        textArea[i].classList.add("present");
+    }
+    
+}
+// Save buttons that send each section to local storage
+$("#8amBtn").on("click", function(){
+    let eightTask = $("#8").val();
+    localStorage.setItem("8am", eightTask);
+})
+$("#9amBtn").on("click", function(){
+    let nineTask = $("#9").val();
+    localStorage.setItem("9am", nineTask);
+})
+$("#10amBtn").on("click", function(){
+    let tenTask = $("#10").val();
+    localStorage.setItem("10am", tenTask);
+})
+$("#11amBtn").on("click", function(){
+    let elevenTask = $("#11").val();
+    localStorage.setItem("11am", elevenTask);
+})
+$("#12pmBtn").on("click", function(){
+    let noonTask = $("#12").val();
+    localStorage.setItem("12pm", noonTask);
+})
+$("#1pmBtn").on("click", function(){
+    let oneTask = $("#13").val();
+    localStorage.setItem("1pm", oneTask);
+})
+$("#2pmBtn").on("click", function(){
+    let twoTask = $("#14").val();
+    localStorage.setItem("2pm", twoTask);
+})
+$("#3pmBtn").on("click", function(){
+    let threeTask = $("#15").val();
+    localStorage.setItem("3pm", threeTask);
+})
+$("#4pmBtn").on("click", function(){
+    let fourTask = $("#16").val();
+    localStorage.setItem("4pm", fourTask);
+})
+$("#5pmBtn").on("click", function(){
+    let fiveTask = $("#17").val();
+    localStorage.setItem("5pm", fiveTask);
+})
+/*
+1. compare, (using if statements), the now value, to the IDs of each div. if it's less than the current time, add "past" class. if it's more than, add "future"
+
+2. on click of the save button in each div, you need to store the value of the adjacent textarea,AND push it to localstorage
+
+3. localStorage.setItem("name of the id","value be the text area"). on load of the page, check if the corresponding localStorage has a value. if yes, then populate the textarea with it. hint: make value = localStorage value
+*/
